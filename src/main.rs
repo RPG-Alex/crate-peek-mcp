52b93302d08cfbdb599398faff7baff528884eca
RPG-Alex <mralexblood@outlook.com>
2025-11-03 21:52:31 +0800

    switching to stdio

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,29 @@
-use rmcp::transport::sse_server::{SseServer, SseServerConfig};
-use tracing_subscriber::{
-    layer::SubscriberExt,
-    util::SubscriberInitExt,
-    {self},
-};
-mod counter;
+use anyhow::Result;
 use counter::Counter;
-const BIND_ADDRESS: &str = "127.0.0.1:8000";
+use rmcp::{ServiceExt, transport::stdio};
+use tracing_subscriber::{self, EnvFilter};
 
+
+
+mod counter;
+/// npx @modelcontextprotocol/inspector cargo run -p mcp-server-examples --example std_io
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "debug".to_string().into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
+async fn main() -> Result<()> {
+
+    // Initialize the tracing subscriber with file and stdout logging
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+        .with_writer(std::io::stderr)
+        .with_ansi(false)
         .init();
 
-    let config = SseServerConfig {
-        bind: BIND_ADDRESS.parse()?,
-        sse_path: "/sse".to_string(),
-        post_path: "/message".to_string(),
-        ct: tokio_util::sync::CancellationToken::new(),
-        sse_keep_alive: None,
-    };
+    tracing::info!("Starting MCP server");
 
-    let (sse_server, router) = SseServer::new(config);
+    // Create an instance of our counter router
+    let service = Counter::new().serve(stdio()).await.inspect_err(|e| {
+        tracing::error!("serving error: {:?}", e);
+    })?;
 
-    // Do something with the router, e.g., add routes or middleware
-
-    let listener = tokio::net::TcpListener::bind(sse_server.config.bind).await?;
-
-    let ct = sse_server.config.ct.child_token();
-
-    let server = axum::serve(listener, router).with_graceful_shutdown(async move {
-        ct.cancelled().await;
-        tracing::info!("sse server cancelled");
-    });
-
-    tokio::spawn(async move {
-        if let Err(e) = server.await {
-            tracing::error!(error = %e, "sse server shutdown with error");
-        }
-    });
-
-    let ct = sse_server.with_service(Counter::new);
-
-    tokio::signal::ctrl_c().await?;
-    ct.cancel();
+    service.waiting().await?;
     Ok(())
 }
